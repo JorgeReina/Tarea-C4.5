9e6ec306f13a175fad6e3afb0d354703c8927867
Jorge Reina Romero <116582548+JorgeReina@users.noreply.github.com>
2022-12-19 17:46:21 +0100

    Add files via upload

--- Ejercicio25.java
+++ Ejercicio25.java
@@ -16,7 +16,42 @@ import java.util.Scanner;
 public class Ejercicio25 {
 	public static void main(String[] args) {
 		 
+		 Scanner datos = new Scanner(System.in);
 		 
+		 System.out.print("Introduzca la altura de la bandera en cm: ");
+		 int altura = datos.nextInt();
+		 
+		 System.out.print("Ahora introduzca la anchura: ");
+		 int anchura = datos.nextInt();
+		 
+		 System.out.print("¿Quieres escudo bordado? (s/n): ");
+		 String bordado = datos.next();
+		 
+		 int cm2 = altura * anchura;
+		 
+		 double precio = cm2 * 0.01;
+		 
+		 double escudo = 0;
+		 
+		 double envio = 3.25;
+		 
+		 String texto = "";
+		 
+		if (bordado.equals("s")) {
+      texto = "Con escudo:";
+      escudo = 2.50;
+    } else {
+      texto = "Sin escudo:";
+      escudo = 0;
+    }
+			
+			double total = precio + escudo + envio;
+		 
+		 System.out.println("Gracias. Aquí tiene el desglose de su compra.");
+		 System.out.printf("Bandera de %s cm2: %s Euros\n", cm2, precio);
+		 System.out.println(texto + " " + escudo + "Euros");
+		 System.out.printf("Gastos de envio: %s Euros\n", envio);
+		 System.out.printf("Total: %.2f Euros", total);
 		 
  }
 }
